8be7c01470a4120d3a83e9ee445ac73e6a9d719a
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-02-28 14:54:17 +0500

    change log out

--- app/(secret)/components/item-box.tsx
+++ app/(secret)/components/item-box.tsx
@@ -6,12 +6,19 @@ import {
     DropdownMenuContent,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { SignOutButton, useUser } from "@clerk/nextjs";
+import { SignOutButton, useClerk, useUser } from "@clerk/nextjs";
 import { ChevronsLeftRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function ItemBox() {
     const { user } = useUser();
+    const { signOut } = useClerk();
+    const router = useRouter();
 
+    const handleSignOut = async () => {
+        await signOut();
+        router.push("/");
+    };
     return (
         <div className="dark:bg-neutral-800 bg-neutral-400/50 w-full py-2">
             <div className="pl-4">
@@ -37,20 +44,20 @@ export default function ItemBox() {
                         </DropdownMenuTrigger>
                     </div>
                     <DropdownMenuContent
-                        className="w-80 z-[100000]"
+                        className="w-80 z-100000"
                         align="start"
                         alignOffset={11}
                         forceMount
                     >
                         <div className="px-1 py-2 bg-neutral-800 rounded-[5px] flex flex-col text-white">
-                            <span className="mb-1">
+                            <span className="mb-1 flex justify-center items-center">
                                 {user?.firstName} Notion
                             </span>
                             <div
-                                className="flex justify-center border-t border-neutral-500 mt-1 "
+                                className="flex justify-center border-t border-neutral-500 mt-1 bg-[crimson] cursor-pointer py-1 rounded-[5px] hover:opacity-75"
                                 role="button"
                             >
-                                <SignOutButton>Log out</SignOutButton>
+                                <button onClick={handleSignOut}>Log out</button>
                             </div>
                         </div>
                     </DropdownMenuContent>
